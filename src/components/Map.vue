f11e67e61e5535a398b621647abce160aac6a136
DarKsandr <gdarksandr@gmail.com>
2025-02-03 00:59:52 +0400

    Make move items

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,6 +2,7 @@
   import type Country from "../interfaces/Country.ts";
   import InlineSvg from 'vue-inline-svg';
   import type CountryItem from "../interfaces/CountryItem.ts";
+  import interact from 'interactjs';
 
   defineProps<{
     country: Country;
@@ -11,6 +12,38 @@
   const emit = defineEmits<{
     countryEnter: [item: CountryItem|null]
   }>();
+
+  const setIndex = (target: HTMLElement, index: string) => {
+    target.style.zIndex = index;
+  }
+
+  interact('.map-item').draggable({
+    listeners: {
+      move (event) {
+        const position = { 
+          x: +event.target.style.left.replace('px', ''), 
+          y: +event.target.style.top.replace('px', ''),
+        };
+
+        position.x += event.dx;
+        position.y += event.dy;
+
+        const target = event.target;
+
+        target.style.top = `${position.y}px`;
+        target.style.left = `${position.x}px`;
+        setIndex(target, '2');
+      },
+      end(event){
+        setIndex(event.target, '1');
+      }
+    },
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent' 
+      })
+    ]
+  });
 </script>
 
 <template>
@@ -34,5 +67,6 @@
   }
   .map-item {
     position: absolute;
+    z-index: 1;
   }
 </style>
