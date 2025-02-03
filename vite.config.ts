285dc007b98328b74e8b5604e5beda6c9502abe3
DarKsandr <gdarksandr@gmail.com>
2025-02-03 23:09:47 +0400

    Add preview config && copy static files

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteStaticCopy({
+    targets: [
+      {
+        src: 'src/countries/files',
+        dest: 'src/countries'
+      }
+    ]
+  })],
   server: {
     host: true,
     port: 5173
+  },
+  preview: {
+    host: true,
+    port: 4173
   }
 })
