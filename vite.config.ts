1b28a7c9c0b11c776863300f5749719569946e0d
DarKsandr <gdarksandr@gmail.com>
2025-02-04 16:37:05 +0400

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/countries-map/',
   server: {
     host: true,
     port: 5173
