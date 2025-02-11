fc9f98a4dfaf8b4b37d59767572faa0505305870
DarKsandr <gdarksandr@gmail.com>
2025-02-11 12:21:16 +0400

    Add env

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: '/countries-map/',
-  server: {
-    host: true,
-    port: 5173
-  },
-  preview: {
-    host: true,
-    port: 4173
-  }
-})
+export default ({ mode }: {mode: string}) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [vue()],
+    base: process.env.VITE_BASE_URL,
+    server: {
+      host: true,
+      port: 5173
+    },
+    preview: {
+      host: true,
+      port: 4173
+    }
+  })
+};
